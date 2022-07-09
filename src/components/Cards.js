b3d4d42b817712e48efe13b0667c722e97d947c8
khanshorif331 <khanshorif331@gmail.com>
2022-07-09 23:39:44 +0600

    updated vaalue

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,7 +6,7 @@ import { Stack } from '@mui/material'
 const Cards = () => {
 	return (
 		<Container>
-			<h1>Hello Joonogon</h1>
+			<h1>Hello Joonogon Are you there?</h1>
 			<Stack direction='row' spacing='50px' sx={{ margin: '0 auto' }}>
 				<Card></Card>
 				<Card></Card>
